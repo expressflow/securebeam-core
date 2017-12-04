9dcdfcb534bde4694323b94465db4c04fb78c3db
Martin Vasko <martin@expressflow.com>
2017-12-04 23:14:33 +0100

    Working on encryption

---
+++ ios/SecureBeam Core/Pods/Target Support Files/Pods-SecureBeam CoreTests/Pods-SecureBeam CoreTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SecureBeam_CoreTests : NSObject
+@end
+@implementation PodsDummy_Pods_SecureBeam_CoreTests
+@end
--- ios/SecureBeam Core/SecureBeam Core/ViewController.swift
+++ ios/SecureBeam Core/SecureBeam Core/ViewController.swift
@@ -31,6 +31,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func touchedGenerateKeyPair(_ sender: Any) {
+        let tag = "com.securebeam.core".data(using: .utf8)!
+        let attributes: [String: Any] =
+            [kSecAttrKeyType as String:            kSecAttrKeyTypeRSA,
+             kSecAttrKeySizeInBits as String:      2048,
+             kSecPrivateKeyAttrs as String:
+                [kSecAttrIsPermanent as String:    true,
+                 kSecAttrApplicationTag as String: tag]
+        ]
+        var error: Unmanaged<CFError>?
+        do {
+            guard let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
+                throw error!.takeRetainedValue() as Error
+            }
+            let publicKey = SecKeyCopyPublicKey(privateKey)
+        } catch {
+            print(error)
+        }
+    }
     
     @IBAction func touchedBrowseDocuments(_ sender: Any) {
         performSegue(withIdentifier: "browseDocuments", sender: nil)
