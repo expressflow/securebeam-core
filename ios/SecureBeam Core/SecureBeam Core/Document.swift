68f19e62d16fa616e7228956275ce341e135c5fb
Martin Vasko <martin@expressflow.com>
2017-12-04 21:52:40 +0100

    [ios] Initial setup

--- ios/SecureBeam Core/SecureBeam Core/Document.swift
+++ ios/SecureBeam Core/SecureBeam Core/Document.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2017 expressFlow GmbH. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
--- ios/SecureBeam Core/SecureBeam Core/ViewController.swift
+++ ios/SecureBeam Core/SecureBeam Core/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var document: UIDocument?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                print(self.document?.fileURL.lastPathComponent)
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func touchedBrowseDocuments(_ sender: Any) {
+        performSegue(withIdentifier: "browseDocuments", sender: nil)
+    }
+    
 }
 
